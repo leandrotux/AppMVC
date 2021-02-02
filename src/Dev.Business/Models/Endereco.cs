eb09136932993dde95e8a06dafeeb07240646eff
Leandro Dourado <leandrotux@hotmail.com>
2021-02-02 17:44:11 -0300

    Created project and models

---
+++ src/Dev.Business/Models/Endereco.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models
+{
+    public class Endereco : Entity
+    {
+        public Guid FornecedorId { get; set; }
+
+        
+        public string Logradouro { get; set; }
+
+        
+        public string Numero { get; set; }
+
+        public string Complemento { get; set; }
+
+        
+        public string Cep { get; set; }
+
+       
+        public string Bairro { get; set; }
+
+        
+        public string Cidade { get; set; }
+
+        
+        public string Estado { get; set; }
+
+        /* EF Relation */
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
---
+++ src/Dev.Business/Models/TipoFornecedor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models
+{
+    public enum TipoFornecedor
+    {
+        Física = 1,
+         Júrdica = 2
+    }
+}
