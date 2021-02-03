985ee859fca6ef7745ecb8e565899e4b1ccb68bc
Leandro Dourado <leandrotux@hotmail.com>
2021-02-03 06:18:57 -0300

    Add EntityFramework

---
+++ src/Dev.Data/Context/DevDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dev.Data.Context
+{
+    public class DevDbContext : DbContext
+    {
+        public DevDbContext()
+        {
+
+        }
+    }
+}
