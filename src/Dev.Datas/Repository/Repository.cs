8517fc364f759c1465e4565ad10b47258dbb340c
Leandro Dourado <leandrotux@hotmail.com>
2021-02-04 17:41:45 -0300

    Created repository generic

---
+++ src/Dev.Datas/Repository/Repository.cs
@@ -0,0 +1,65 @@
+﻿using AppMvcBasica.Models;
+using Dev.Business.Interfaces;
+using Dev.Datas.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev.Datas.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected readonly DevDbContext db;
+        protected readonly DbSet<TEntity> dbSet;
+
+        public async Task<IEnumerable<TEntity>> Search(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await dbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task<List<TEntity>> GetAll()
+        {
+            return await dbSet.ToListAsync();
+        }
+
+        public virtual async Task<TEntity> GetForId(Guid id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public virtual async Task Add(TEntity entity)
+        {
+            dbSet.Add(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Update(TEntity entity)
+        {
+            dbSet.Update(entity);
+            await SaveChanges();
+        }
+
+
+        public virtual async Task Delete(Guid id)
+        {
+            
+            //dbSet.Remove(await dbSet.FindAsync(id));
+            dbSet.Remove(new TEntity { Id = id });
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await db.SaveChangesAsync();
+        }
+        
+        public async void Dispose()
+        {
+            db?.Dispose();
+        }
+    }
+}
