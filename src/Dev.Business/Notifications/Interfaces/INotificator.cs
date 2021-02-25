f661d8dcf41f7895d4659a7606f902347780a2fe
Leandro Alves Dourado Arcanjo <leandrotux@hotmail.com>
2021-02-25 06:43:24 -0300

    Created Inteface notifications

---
+++ src/Dev.Business/Notifications/Interfaces/INotificator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dev.Business.Notifications.Interfaces
+{
+    public interface INotificator
+    {
+        bool HasNotification();
+
+        List<Notification> GetNotifications();
+
+        void Handle(Notification notification);
+    }
+}
---
+++ src/Dev.Business/Notifications/Notification.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dev.Business.Notifications
+{
+    public class Notification
+    {
+    }
+}
